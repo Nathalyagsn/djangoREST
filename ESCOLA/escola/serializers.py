7cec80d9c8d349f9d9b786189f0fd95e87db333e
Nathalya Silva <nathalya.gsn@gmail.com>
2025-04-02 16:16:53 -0300

    adicionando recursos de listar matriculas

--- ESCOLA/escola/serializers.py
+++ ESCOLA/escola/serializers.py
@@ -25,8 +25,8 @@ class ListaMatriculasEstudanteSerializer(serializers.ModelSerializer):
     def get_periodo(self, obj):
         return obj.get_periodo_display()
     
-class ListaMatriculasCurso(serializers.ModelSerializer):
+class ListaMatriculasCursoSerializer(serializers.ModelSerializer):
     estudante_nome = serializers.ReadOnlyField(source = 'estudante.nome')
     class Meta:
         model = Matricula
-        fields = ['estudante']
+        fields = ['estudante_nome']
