b975d7b35c10af9580d8e1ca2fcf7e31b5bb27e8
Nathalya Silva <nathalya.gsn@gmail.com>
2025-05-05 17:07:18 -0300

    testando requisição get para listar matricula

--- ESCOLA/escola/tests/test_matriculas.py
+++ ESCOLA/escola/tests/test_matriculas.py
@@ -2,9 +2,38 @@ from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
+from escola.models import Matricula, Estudante, Curso
 
 class MatriculasTestCase(APITestCase):
     def setUp(self):
         self.usuario = User.objects.create_superuser(username='admin', password='admin')
         self.url = reverse('Matriculas-list')
         self.client.force_authenticate(user=self.usuario)
+        self.estudante = Estudante.objects.create(
+            nome='João da Silva',
+            cpf='12345678900',
+            data_nascimento='2000-01-01'
+        )
+
+        self.curso = Curso.objects.create(
+            codigo='C001',
+            descricao='Curso de Python',
+            nivel='B'
+        )
+
+        Matricula.objects.create(
+            estudante=self.estudante,
+            curso=self.curso,
+            periodo='Noturno'
+        )
+
+        Matricula.objects.create(
+            estudante=self.estudante,
+            curso=self.curso,
+            periodo='Diurno'
+        )
+
+    def test_de_requisicao_get_para_listar_matricula(self):
+        """Teste de requisição GET"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
