48c7bde4ce6142ef6c2c190620677ff97141036b
Nathalya Silva <nathalya.gsn@gmail.com>
2025-05-05 16:33:24 -0300

    testando a requisição GET

--- ESCOLA/escola/tests/test_estudantes.py
+++ ESCOLA/escola/tests/test_estudantes.py
@@ -2,9 +2,30 @@ from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
+from escola.models import Estudante
 
 class EstudantesTestCase(APITestCase):
     def setUp(self):
         self.usuario = User.objects.create_superuser(username='admin', password='admin')
         self.url = reverse('Estudantes-list')
         self.client.force_authenticate(user=self.usuario)
+        self.estudante_01 = Estudante.objects.create(
+            nome = 'Teste estudante UM',
+            email = 'testeestudanteUM@gmail.com',
+            cpf = '51225544041',
+            data_nascimento = '2004-01-02',
+            celular = '81 99999-9999'
+        )
+        self.estudante_02 = Estudante.objects.create(
+            nome = 'Teste estudante DOIS',
+            email = 'testeestudante01@gmail.com',
+            cpf = '75102534033',
+            data_nascimento = '2003-01-02',
+            celular = '81 98888-8888'
+        )
+
+    def test_requisicao_get_para_listar_estudante(self):
+        """Teste de requisição GET"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        
